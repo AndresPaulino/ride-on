d18b369879cf0e293524a9281fc50024ceef010c
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-09 15:20:35 -0400

    Style card components - Add dummy data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-function Card() {
+function Card({ item }) {
+
+    console.log(item);
   return (
     <div className='w-full p-5'>
-      <a href='/' className='w-100 overflow-hidden bg-center bg-no-repeat bg-cover rounded-xl'>
-        <div className='p-8 pt-40 text-white bg-black bg-opacity-40'>
-          <h5 className='text-2xl font-bold'>MIAMI</h5>
+      <a class='block h-56 group' href='/blog'>
+        <div class='relative flex items-end h-full transition bg-white border-4 border-black group-hover:-translate-x-2 group-hover:-translate-y-2 rounded-3xl group-hover:shadow-[8px_8px_0_0_#000] p-8'>
+          <div class='lg:group-hover:opacity-0 lg:group-hover:absolute'>
+            
+                      <p class='mt-4 text-xl font-bold sm:text-2xl'>{item}</p>
+          </div>
+
+          <div class='absolute opacity-0 lg:group-hover:opacity-100 lg:group-hover:relative'>
+                      <p class='text-2xl font-bold'>{item}</p>
+
+            <p class='mt-4 text-lg font-medium leading-relaxed'>
+              Datepicker input using Flatpickr with a button that clears the selected date.
+            </p>
+          </div>
         </div>
       </a>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import NavigationBar from '../../components/NavigationBar';
 import Card from '../../components/Card';
+import axios from 'axios';
 
 function Home() {
+  const [data, setData] = useState(['Miami', 'California', 'Toronto']);
+    const [loading, setLoading] = useState(true);
+    
+    console.log(data);
+
+  // useEffect(() => {
+  //     const fetchData = async () => {
+  //         const result = await axios(
+
+  //             'https://api.opendota.com/api/heroes'
+  //         );
+  //         console.log(result.data);
+  //         setData(result.data);
+  //         setLoading(false);
+  //     }
+  //     fetchData();
+  // }, []);
+
   return (
     <div>
       <NavigationBar />
-      <div className='cards flex'>
-        <Card />
-        <Card />
-        <Card />
+      <div className='cards flex justify-between px-10 flex-col md:flex-row'>
+        {data.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
       </div>
     </div>
   );
