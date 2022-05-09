be0d872f06f115f6ab6938ad7f0b780ed0917439
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-09 15:43:06 -0400

    Fix styling and responsive design

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -51,7 +51,7 @@ export default function Example() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-quinary text-white' : 'text-gray-300 hover:text-white',
+                          item.current ? 'bg-quinary hover:bg-quinaryDark text-white' : 'text-gray-300 hover:text-white',
                           'px-3 py-2 rounded-md text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -68,10 +68,11 @@ export default function Example() {
                   <div>
                     <Menu.Button className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
                       <span className='sr-only'>Open user menu</span>
+                      {/* Profile picture */}
                       <img
                         className='h-8 w-8 rounded-full'
                         src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                        alt=''
+                        alt='profile'
                       />
                     </Menu.Button>
                   </div>
@@ -84,7 +85,7 @@ export default function Example() {
                     leaveFrom='transform opacity-100 scale-100'
                     leaveTo='transform opacity-0 scale-95'
                   >
-                    <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                    <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10'>
                       <Menu.Item>
                         {({ active }) => (
                           <a
