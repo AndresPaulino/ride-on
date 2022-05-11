fcea2326b680caea692724453d57849d95ab5b3b
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-11 00:15:33 -0400

    Add create user and set current user function

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,22 +1,31 @@
-import { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../FirebaseApp';
 
 const AuthContext = React.createContext();
 
 export function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
-    
 
 export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState();
 
-    const value = {
-        currentUser
-    }
+  // Create user function
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
 
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-  ) 
+    // Set current user
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
