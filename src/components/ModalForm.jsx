26ec89d326b7201e24bc9c10c8cc5bb4007dd9c0
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-17 12:21:21 -0400

    Add submit logic

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import { useRef } from 'react';
+import { useStateContext } from '../context/StateContext';
+import axios from 'axios';
 
 function ModalForm() {
+  const { user, failedAuth } = useStateContext();
+
+  const titleRef = useRef();
+  const addressRef = useRef();
+  const dateRef = useRef();
+  const timeRef = useRef();
+  const descriptionRef = useRef();
+    const fromRef = useRef();
+    
+    const { user_name, profile_img } = user;
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      
+      const title = titleRef.current.value;
+      const address = addressRef.current.value;
+      const date = dateRef.current.value;
+      const time = timeRef.current.value;
+      const description = descriptionRef.current.value;
+      const from = fromRef.current.value;
+      
+      const data = {
+          title,
+          address,
+          date,
+          time,
+          description,
+          from,
+          user_name,
+          profile_img
+      };
+      
+  };
   return (
     <div className='p-4 max-w-lg mx-auto bg-gray-900 shadow-2xl'>
-      <form className='w-full bg-gray-900 shadow-4xl flex-col justify-center align-middle items-center'>
+      <form
+        className='w-full bg-gray-900 shadow-4xl flex-col justify-center align-middle items-center'
+        onSubmit={handleSubmit}
+      >
         {/* Title Input */}
         <div>
           <label className='text-white mb-2'>Title</label>
-          <input className='w-full bg-white p-2 rounded-lg' type='text' placeholder='Title' />
+          <input className='w-full bg-white p-2 rounded-lg' type='text' placeholder='Title' name='ride_title' ref={titleRef} />
         </div>
         {/* Ride From Input */}
         {/* Date Input */}
@@ -16,33 +54,43 @@ function ModalForm() {
           {/* Ride From */}
           <div className='flex-col my-4 max-w-[8rem]'>
             <label className='text-white mb-2'>Ride From</label>
-            <input className='w-full bg-white p-2 rounded-lg' type={'search'} placeholder='Ride From' />
+            <input
+              className='w-full bg-white p-2 rounded-lg'
+              type={'search'}
+              placeholder='Ride From'
+              name='ride_from'
+              ref={fromRef}
+            />
           </div>
           {/* Date */}
           <div className='flex-col my-4 max-w-[10rem]'>
             <label className='text-white mb-0 block'>Date</label>
-            <input className='w-full bg-white p-2 rounded-lg' type={'date'} placeholder='Date' />
+            <input className='w-full bg-white p-2 rounded-lg' type={'date'} placeholder='Date' ref={dateRef} />
           </div>
           {/* Time */}
           <div className='max-w-[8rem] my-4'>
             <label className='text-white mb-2 flex-col '>Time</label>
-            <input className='w-full bg-white p-2 rounded-lg' type={'time'} placeholder='Time' />
+            <input className='w-full bg-white p-2 rounded-lg' type={'time'} placeholder='Time' ref={timeRef} />
           </div>
         </div>
         {/* Address */}
         <div>
           <label className='text-white mb-2'>Address</label>
-          <input className='w-full bg-white p-2 rounded-lg' type={'address'} placeholder='Address' />
+          <input className='w-full bg-white p-2 rounded-lg' type={'address'} placeholder='Address' ref={addressRef}/>
         </div>
         {/* Description */}
         <div className='flex-col justify-start align-top items-start my-4'>
           <label className='text-white mb-2'>Description</label>
-          <input className='w-full h-36 bg-white p-2 rounded-lg' type={'description'} placeholder='Description' />
+          <input className='w-full h-36 bg-white p-2 rounded-lg' type={'description'} placeholder='Description' ref={descriptionRef} />
         </div>
         {/* Buttons */}
         <div className='flex justify-between my-4'>
-          <button className='w-full mr-2 bg-quinary hover:bg-quinaryDark hover:text-textColor p-2 rounded-lg'>Cancel</button>
-          <button className='w-full ml-2  bg-quinary hover:bg-quinaryDark hover:text-textColor p-2 rounded-lg'>Create Ride</button>
+          <button className='w-full mr-2 bg-quinary hover:bg-quinaryDark hover:text-textColor p-2 rounded-lg'>
+            Cancel
+          </button>
+          <button className='w-full ml-2  bg-quinary hover:bg-quinaryDark hover:text-textColor p-2 rounded-lg'>
+            Create Ride
+          </button>
         </div>
       </form>
     </div>
