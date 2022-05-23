6cd8eb11e07965b70415fa19b0092ef1c11bf254
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-23 11:19:56 -0400

    Import dashboard images

--- src/components/Dashboard/DashboardMain.jsx
+++ src/components/Dashboard/DashboardMain.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { useStateContext } from '../../context/StateContext';
 import { Link } from 'react-router-dom';
 import MyRides from './MyRides';
+import group from '../../assets/images/group.jpg';
+import solo from '../../assets/images/motorcycle_solo.jpg';
 
 function DashboardMain() {
   const { user } = useStateContext();
-  const background = '../../assets/images/orange_bg.jpg';
 
   return (
     <main className='bg-gray-800 w-full h-screen sm:pl-[10rem]'>
@@ -31,15 +32,18 @@ function DashboardMain() {
       <div className='flex justify-between bg-white py-5 mx-14 rounded drop-shadow-all'>
         <Link
           to='/rides'
-          className='flex flex-col items-center justify-end rounded h-[10rem] w-1/3 p-4 m-2 bg-quinary text-gray-900 hover:bg-quinaryDark border-gray-300 hover:text-textColor align-bottom drop-shadow-all'
+          className='flex flex-col items-center justify-end rounded h-[10rem] w-1/3 p-4 m-2 bg-quinary border-gray-300 text-textColor align-bottom drop-shadow-all bg-center bg-cover bg-no-repeat font-semibold shadow-md transition-all hover:scale-105'
+          style={{
+            backgroundImage: `url('${solo}')`,
+          }}
         >
           <h1>Find Rides</h1>
         </Link>
         <Link
           to='/rides'
-          className='flex flex-col items-center justify-end rounded h-[10rem] w-2/3 p-4 m-2 bg-quinary text-gray-900 hover:bg-quinaryDark border-gray-300 hover:text-textColor drop-shadow-all'
+          className='flex flex-col items-center justify-end rounded h-[10rem] w-2/3 p-4 m-2 border-gray-300 text-textColor drop-shadow-all bg-center bg-cover bg-no-repeat font-semibold shadow-md transition-all hover:scale-105'
           style={{
-            backgroundImage: `url(${background})`,
+            backgroundImage: `url('${group}')`,
           }}
         >
           <h1>Connect With Others</h1>
