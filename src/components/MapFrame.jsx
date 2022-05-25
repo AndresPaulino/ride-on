f99ad44cf3f77e4b0b0c15c6ffcadfb7ca23f43d
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-25 08:53:31 -0400

    Fix styling

--- src/components/MapFrame.jsx
+++ src/components/MapFrame.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 
 function MapFrame() {
   const [zoom, setZoom] = useState(13);
-  const [lat, setLat] = useState(51.505);
-  const [lng, setLng] = useState(-0.09);
+  const [lat, setLat] = useState(25.761681);
+  const [lng, setLng] = useState(-80.191788);
   const [markers, setMarkers] = useState([]);
 
   return (
     <div className='leaflet-container ml-14'>
-      <MapContainer center={[42.585444, 13.257684]} zoom={zoom} scrollWheelZoom={true}>
-        <TileLayer
-          url='https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=EgISFZBX6b1lb24zEhEd'
-        />
+      <MapContainer center={[lat, lng]} zoom={zoom} scrollWheelZoom={true}>
+        <TileLayer url='https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=EgISFZBX6b1lb24zEhEd' />
       </MapContainer>
     </div>
   );
