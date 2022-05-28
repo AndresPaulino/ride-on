fe7983784bb25198a5f16778f20ca7a392f900f8
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-28 15:54:23 -0400

    Add handle upload picture functions

--- src/components/Dashboard/ProfilePicture.jsx
+++ src/components/Dashboard/ProfilePicture.jsx
@@ -7,26 +7,33 @@ function ProfilePicture() {
   const { user } = useStateContext();
 
   const [image, setImage] = useState(user.profile_img);
+  const [imageFile, setImageFile] = useState(null);
 
-  const handleImageChange = (e) => {
-    console.log(e.target.files[0].name);
-    setImage(e.target.files[0]);
+  const handleImagePreview = (event) => {
+    setImageFile(event.target.files[0]);
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(event.target.files[0]);
+    setImage(event.target.files[0]);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      user_id: user.id,
-      profile_img: image.name,
-    };
-    axios
-      .post('http://localhost:8080/update-profile-img', data)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  // Handle upload for logged in user
+  const handleSubmitUpload = (event) => {
+    const formData = new FormData();
+    formData.append('file', imageFile);
+    formData.append('upload_preset', 'hos3vnjf');
+
+    console.log(imageFile);
+
+    axios.post('https://api.cloudinary.com/v1_1/elysium-devleopers/image/upload', formData).then((res) => {
+      console.log(res);
+    });
+
+    event.preventDefault();
   };
 
   return (
@@ -34,7 +41,10 @@ function ProfilePicture() {
       <div className='flex flex-col align-middle justify-center items-center bg-white w-full max-w-lg rounded py-5 drop-shadow-all'>
         <h2 className='text-primary font-semibold mb-5'>Change Profile Image</h2>
         <img className='w-48 h-auto rounded-full mb-5' src={image} alt='profile' />
-        <form onSubmit={handleSubmit} className='flex flex-col w-full justify-center align-middle items-center gap-5'>
+        <form
+          onSubmit={handleSubmitUpload}
+          className='flex flex-col w-full justify-center align-middle items-center gap-5'
+        >
           <label
             htmlFor='profile_img'
             className='text-primary bg-quinary py-2 px-4 rounded hover:bg-quinaryDark hover:text-textColor cursor-pointer drop-shadow-all'
@@ -42,7 +52,7 @@ function ProfilePicture() {
             Choose Picture
           </label>
           <input
-            onChange={handleImageChange}
+            onChange={handleImagePreview}
             type='file'
             accept='image/*'
             name='profile_img'
